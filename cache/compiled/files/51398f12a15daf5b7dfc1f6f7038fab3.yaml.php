1a9e623a3c71450a47d159e1cae98ce4c662798e
ron <ronakjp86@gmail.com>
2017-06-08 16:16:53 +0530

    cache deletions and log update

--- cache/compiled/files/51398f12a15daf5b7dfc1f6f7038fab3.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'D:/Workarea/xampp/htdocs/grav/grav-template/user/config/security.yaml',
-    'modified' => 1496651599,
-    'data' => [
-        'salt' => 'KZ3n0LXRs94bDg'
-    ]
-];
